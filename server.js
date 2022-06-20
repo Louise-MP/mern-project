59c91b68eab4058e049567fa1cb265571874fa47
Louise Mpili <louise.mpili@uniqueheritage.fr>
2022-06-20 19:23:52 +0200

    cors

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ const corsOptions ={
 }
 
 const allowCrossDomain = function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', "*");
+  res.header('Access-Control-Allow-Origin', "https://racoont.netlify.app");
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
