6f49914290adb8db4557c97e8c0ae6912dff9b76
Louise Mpili <louise.mpili@uniqueheritage.fr>
2022-06-21 05:38:31 +0200

    cors

--- server.js
+++ server.js
@@ -19,14 +19,21 @@ const app = express();
 
 const corsOptions ={
   origin: process.env.CLIENT_URL || "https://racoont.netlify.app",
+  "Access-Control-Allow-Origin": "https://racoont.netlify.app",
   credentials: true,
   headers: ["Access-Control-Allow-Origin","Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type", "CORELATION_ID"],
-  allowedHeaders: 'Origin, X-Requested-With, x-access-token, role, Content, Accept, Content-Type, Authorization, *',
+  allowedHeaders: 'Origin, X-Requested-With, x-access-token, role, Content, Accept, Content-Type, Authorization',
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   preflightContinue: false
 }
  
 app.use(cors(corsOptions));
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept");
+  next();
+})
 
 
 // body parser (indispensable pour traiter les données qui vont transiter lors des requetes)
