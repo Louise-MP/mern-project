1b0c6558e42d01fc7c675c5acb4005913c575f45
Louise Mpili <louise.mpili@uniqueheritage.fr>
2022-06-21 05:53:31 +0200

    cors

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ const corsOptions ={
 app.use(cors(corsOptions));
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "https://racoont.netlify.app");
   res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept");
   next();
 })
