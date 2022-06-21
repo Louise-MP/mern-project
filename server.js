6036122155d414348cb698c48f67edb39166990c
Louise Mpili <louise.mpili@uniqueheritage.fr>
2022-06-21 03:43:53 +0200

    cors

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const app = express();
 // solution : activer les crdentials dans axios ET dans le cors 
 
 const corsOptions ={
-  origin: '*',
+  origin: 'process.env.CLIENT_URL',
   credentials: true,
   allowedHeaders: 'Origin, X-Requested-With, x-access-token, role, Content, Accept, Content-Type, Authorization, *',
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
