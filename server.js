ca7e0faccf16c2a73bba0c69c046614e08be29b5
Louise Mpili <louise.mpili@uniqueheritage.fr>
2022-06-21 05:48:08 +0200

    cors

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ const corsOptions ={
 app.use(cors(corsOptions));
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", process.env.CLIENT_URL);
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept");
   next();
 })
