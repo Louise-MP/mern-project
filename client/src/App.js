d72c848960db89347a56d9bf2f30b48b948996f5
Louise Mpili <louise.mpili@uniqueheritage.fr>
2022-06-22 17:58:03 +0200

    cors

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,11 @@ const App = () => {
         method: "get",
         url: `${process.env.REACT_APP_API_URL}jwtid`, // quand il y a un get /jwtid, go dans l'api récup l'id du user 🤷🏽‍♀️ grace à axios
         withCredentials: true,
-        headers: {"Content-Type": "application/json"},
+        headers: {
+          "Content-Type": "application/json",
+          "Accept" : "application/json",
+          "Authorization": "https://racoont.netlify.app"
+        },
         // on ne passe pas de data car c'est un get : on veut recupérer des informations  
       })
         .then((res) => {
